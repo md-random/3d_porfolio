8e5f6f860821ca1271717e1975ada4714420520c
Michael Degginger <mdegginger@protonmail.com>
2023-12-25 17:35:56 -0700

    Island set up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from '../components/Loader'
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full  h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ neaqr: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+            <directionalLight />
+            <ambientLight />
+            <pointLight />
+            <spotLight />
+            <hemisphereLight />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
